3afe6058afa6ef7da5f75eb1d99449864dc57c8b
FabioZavataro <fabiozavataro@gmail.com>
2024-09-24 11:26:33 +0200

    First Commit

---
+++ es3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $grandezza = rand(16,60);
+        echo "<p style='font-size: " . $grandezza . "px'>SONO UN PARAGRAFO</p>"
+    ?>
+</body>
+</html>
